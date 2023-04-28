1c24d93bd9d4f9958fca78115a2aba2e8ffc0cf0
Sebas Dev <sebasariasdev@gmail.com>
2023-04-28 23:14:06 +0200

    Added loading spinner

--- dom/load-details.js
+++ dom/load-details.js
@@ -27,12 +27,22 @@ const fetchDetails = async (urlString) => {
 };
 
 window.addEventListener('DOMContentLoaded', async () => {
+  document.querySelector('#root').innerHTML = `
+  <div class="text-center">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <br>
+    <h4>Loading Pokemon Data...</h4>
+  </div>
+  `;
+
   const currentUrl = localStorage.getItem('pokemonUrl');
   console.log('URL', currentUrl);
   const details = await fetchDetails(currentUrl);
   const htmlContent = renderDetailsHtml(details);
+
   document.querySelector('#root').innerHTML = htmlContent;
-  console.log(details);
 });
 
 const renderDetailsHtml = ({
@@ -53,25 +63,32 @@ const renderDetailsHtml = ({
       <div class="col-3" style="text-align: center;">
         <img
           src="${pokemonImg}"
+          class="img-fluid w-100"
           />
       </div>
 
       <div class="col-9">
         <div class="row mb-3">
           <div class="col-4">
-            <div class="stats-card ${hpStats.base_stat <= 50 ? 'bad-stat' :'good-stat'}">
+            <div class="stats-card ${
+              hpStats.base_stat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
               <p class="stat-title">Hp:</p>
               <p class="stat-number">${hpStats.base_stat}</p>
             </div>
           </div>
           <div class="col-4">
-            <div class="stats-card ${attackStats.base_stat <= 50 ? 'bad-stat' :'good-stat'}">
+            <div class="stats-card ${
+              attackStats.base_stat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
               <p class="stat-title">Attack:</p>
               <p class="stat-number">${attackStats.base_stat}</p>
             </div>
           </div>
           <div class="col-4">
-            <div class="stats-card ${defenseStat.base_stat <= 50 ? 'bad-stat' :'good-stat'}">
+            <div class="stats-card ${
+              defenseStat.base_stat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
               <p class="stat-title">Defense:</p>
               <p class="stat-number">${defenseStat.base_stat}</p>
             </div>
@@ -79,19 +96,25 @@ const renderDetailsHtml = ({
         </div>
         <div class="row">
           <div class="col-4">
-            <div class="stats-card ${specialAttStats.base_stat <= 50 ? 'bad-stat' :'good-stat'}">
+            <div class="stats-card ${
+              specialAttStats.base_stat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
               <p class="stat-title">Special Attack:</p>
               <p class="stat-number">${specialAttStats.base_stat}</p>
             </div>
           </div>
           <div class="col-4">
-            <div class="stats-card ${specialDefStats.base_stat <= 50 ? 'bad-stat' :'good-stat'}">
+            <div class="stats-card ${
+              specialDefStats.base_stat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
               <p class="stat-title">Special Defense:</p>
               <p class="stat-number">${specialDefStats.base_stat}</p>
             </div>
           </div>
           <div class="col-4">
-            <div class="stats-card ${speedStats.base_stat <= 50 ? 'bad-stat' :'good-stat'}">
+            <div class="stats-card ${
+              speedStats.base_stat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
               <p class="stat-title">Speed:</p>
               <p class="stat-number">${speedStats.base_stat}</p>
             </div>
