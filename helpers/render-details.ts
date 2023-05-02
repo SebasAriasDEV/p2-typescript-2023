d24c0879199a21df4fba73e625466a779ae09cad
Sebas Dev <sebasariasdev@gmail.com>
2023-05-02 14:36:42 +0200

    Render new pages for each pokemon

--- helpers/render-details.ts
+++ helpers/render-details.ts
@@ -16,13 +16,80 @@ const header = (): string => `
   </head>
 `;
 
-export const render = (pokemon: Pokemon) => {
+export const renderDetails = (pokemon: Pokemon) => {
   return `
     <!DOCTYPE html>
     <html lang="en">
     ${header()}
         <body>
-          <div id="root"></div>
+          <div id="root">
+          <h1>${pokemon.name.toUpperCase()}</h1>
+    <hr />
+
+    <div class="row">
+      <div class="col-3" style="text-align: center;">
+        <img
+          src="${pokemon.image}"
+          class="img-fluid w-100"
+          />
+      </div>
+
+      <div class="col-9">
+        <div class="row mb-3">
+          <div class="col-4">
+            <div class="stats-card ${
+              pokemon.hpStats <= 50 ? 'bad-stat' : 'good-stat'
+            }">
+              <p class="stat-title">Hp:</p>
+              <p class="stat-number">${pokemon.hpStats}</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="stats-card ${
+              pokemon.attackStats <= 50 ? 'bad-stat' : 'good-stat'
+            }">
+              <p class="stat-title">Attack:</p>
+              <p class="stat-number">${pokemon.attackStats}</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="stats-card ${
+              pokemon.defenseStat <= 50 ? 'bad-stat' : 'good-stat'
+            }">
+              <p class="stat-title">Defense:</p>
+              <p class="stat-number">${pokemon.defenseStat}</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <div class="stats-card ${
+              pokemon.specialAttStats <= 50 ? 'bad-stat' : 'good-stat'
+            }">
+              <p class="stat-title">Special Attack:</p>
+              <p class="stat-number">${pokemon.specialAttStats}</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="stats-card ${
+              pokemon.specialDefStats <= 50 ? 'bad-stat' : 'good-stat'
+            }">
+              <p class="stat-title">Special Defense:</p>
+              <p class="stat-number">${pokemon.specialDefStats}</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="stats-card ${
+              pokemon.speedStats <= 50 ? 'bad-stat' : 'good-stat'
+            }">
+              <p class="stat-title">Speed:</p>
+              <p class="stat-number">${pokemon.speedStats}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+          </div>
           <br /><br />
 
           <button onclick="history.back()" class="btn btn-primary"><< Go back</button>
--- index.ts
+++ index.ts
@@ -2,11 +2,24 @@ import { writeFile } from 'fs/promises';
 
 import { loadPokemons } from './apis/fecth-pokemons.js';
 import { render } from './helpers/render-files.js';
+import { renderDetails } from './helpers/render-details.js';
 
 const pokemons = await loadPokemons(50);
 const indexHTML = render(pokemons);
+
+//Render index HTML
 try {
   await writeFile('pokemons.html', indexHTML);
 } catch (error) {
   console.log(error);
+}
+
+//Render pokemon details
+for (const pokemon of pokemons) {
+  try {
+    const detailsHTML = renderDetails(pokemon);
+    await writeFile(`pokemons-details-${pokemon.name}.html`, detailsHTML);
+  } catch (error) {
+    console.log(error);
+  }
 }
