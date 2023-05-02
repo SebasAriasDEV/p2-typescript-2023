60820b0d43bfd141f40da0f2d344c14b4cefd5b0
Sebas Dev <sebasariasdev@gmail.com>
2023-05-02 14:13:44 +0200

    Created module to render details pages

--- index.ts
+++ index.ts
@@ -5,4 +5,8 @@ import { render } from './helpers/render-files.js';
 
 const pokemons = await loadPokemons(50);
 const indexHTML = render(pokemons);
-await writeFile('pokemons.html', indexHTML);
+try {
+  await writeFile('pokemons.html', indexHTML);
+} catch (error) {
+  console.log(error);
+}
